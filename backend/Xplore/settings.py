fe5f733cb336fdbc57165f26041c60de826853e5
Muaaz1802 <syedmuaaz1802@gmail.com>
2025-10-30 03:44:19 +0530

    fix(backned) model ouput

--- backend/Xplore/settings.py
+++
@@ -1,5 +0,0 @@
-# Define where container outputs are stored
-ADDITIONAL_OUTPUTS_ROOT = os.path.join(BASE_DIR, 'outputs')
-
-# Ensure the directory exists
-os.makedirs(ADDITIONAL_OUTPUTS_ROOT, exist_ok=True)
